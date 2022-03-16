3203e27c09e3b7d440f25dd2b3cb6d037c2e296b
c14190036 <100283424+c14190036@users.noreply.github.com>
2022-03-16 22:27:15 +0700

    Final C14190036_01

--- bin/c14190036_01.dart
+++ bin/c14190036_01.dart
@@ -4,12 +4,13 @@ void main(List<String> arguments) {
   stdout.write("Masukan angka : ");
   int angka = int.parse(stdin.readLineSync()!);
 
-  for (int i = angka; i > 0; i--) {
-    if (angka % i == 0) {
-      if (i > 1) {
-        stdout.write("${i}, ");
+  for (int i = 0; i < angka; i++) {
+    if (angka % (i + 1) == 0) {
+      stdout.write("${i + 1}");
+      if (i < angka - 1) {
+        stdout.write(',');
       } else {
-        stdout.write("${i}");
+        stdout.write('');
       }
     }
   }
